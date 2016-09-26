c24ceb0c3a5dcddf29d78113add106d8a2883361
midhruvjaink <blivefoundations@gmail.com>
2016-09-27 00:34:55 +0530

    bug fixes and icon change v1.1

--- res.js
+++ res.js
@@ -1,3 +1,3 @@
 var x = window.location.href;
 var y = x.substring(71, x.length-8);
-document.write('<iframe 	width="600"  height="450"  frameborder="0" style="border:0"src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAUG1jgsBghqRkt8py6Cx_HW5VusvNkm2Q&q=' + y + '" allowfullscreen></iframe>');
+document.write('<iframe 	width="400"  height="400"  frameborder="0" style="border:0"src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAUG1jgsBghqRkt8py6Cx_HW5VusvNkm2Q&q=' + y + '" allowfullscreen></iframe>');
